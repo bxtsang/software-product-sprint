b22b7102230adac962f21fc3dd35b2dfb7e6ea18
Bao Xian TSANG <bxtsang.2018@sis.smu.edu.sg>
2020-06-28 14:07:06 +0800

    handle eventSplitsRestriction test

--- walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -29,6 +29,9 @@ public final class FindMeetingQuery {
       return Arrays.asList(TimeRange.WHOLE_DAY);
     }
 
+    possibleTimes.add(TimeRange.fromStartEnd(TimeRange.START_OF_DAY, TimeRange.getTimeInMinutes(8, 30), false));
+    possibleTimes.add(TimeRange.fromStartEnd(TimeRange.getTimeInMinutes(9, 0), TimeRange.END_OF_DAY, true));
+
     return possibleTimes;
   }
 }
